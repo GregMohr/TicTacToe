3e9d2ddc3a171339123759e6a0faa54ea3a7fade
Greg Mohr <gregmohr2000@yahoo.com>
2018-02-01 08:20:53 -0800

    Initial setup questions in place

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,31 +4,74 @@ x. powerups: gained with every win, winstreaks, combos, losingstreaks, 2 with wi
 - with 1 player, have the AI be theoretically lossless, but only the player gets the powerups
 - destroy opponent's marker, does it use up your turn, i guess it can't or that'd be worthless, unless you could stack powerups and use more than one
 - extra move
+- steal a win, a circular button appears between the two scores, animated spin while removing 1 from opp and adding to player
 x. explosions on move
 x. little musical refrains when clicking things and moving mouse
 x. fanfare on player win
 x. sad otter on comp win
+x. first to win 10 gets the Ultra win, or perhaps lower and there can be higher levels of wins that give you an extra win or power up (user choice)
+x. player name
+x. user experience component? maybe instead of wins..probably not
 */
+let infoElem = document.getElementById("info"),
+    questionElem = document.getElementById("question"),
+    option1Elem = document.getElementById("option1"),
+    option2Elem = document.getElementById("option2"),
+    player1 = new Player("human"),
+    ask,
+    askComponents = [
+        ["Versus", "Human", "Computer", () => player2 = new Player("human"), () => player2 = new Player("computer")],
+        ["Player 1", "Xs", "Os", () => player1.marker = (player2.marker = "o", "x"), () => player1.marker = (player2.marker = "x", "o")]
+    ];
+
 initGame();
 
-function Ask(ask, result1, result2) {
-    this.ask = ask;
+function Player(type) {
+    this.type = type;
+    this.marker;
+}
+
+function Ask(question, option1, option2, result1, result2) {
+    this.question = question;
+    this.option1 = option1;
+    this.option2 = option2;
     this.result1 = result1;
     this.result2 = result2;
 }
 
 function initGame() {
-    let askOpponent = `Versus: <span id="option1" class="option">Human</span> | <span id="option2" class="option">Computer</span>`;
-    getInput(askOpponent);
+    getInputs();
 }
 
-function getInput(ask) {
-    //maybe I can just display the ask, not have an interval, and successively change the click events
-    let info = document.getElementById("info");
-    info.innerHTML = ask;
-    //start wait interval
-    //both option clicks will
-    //- include a clearInterval
-    //- set a global variable
-    //- 
+function getInputs() {
+    if (!askComponents.length) {
+        console.dir(player1);
+        console.dir(player2);
+        infoElem.innerHTML = "";
+        return;
+    }
+    ask = new Ask(...askComponents.shift());
+    showAsk(ask);
+}
+
+function showAsk() {
+    questionElem.innerHTML = ask.question;
+    option1Elem.innerHTML = ask.option1;
+    option2Elem.innerHTML = ask.option2;
+    option1Elem.addEventListener("click", optionClick1 = function() { optionClick(ask.result1) });
+    option2Elem.addEventListener("click", optionClick2 = function() { optionClick(ask.result2) });
+}
+
+function optionClick(callback) {
+    callback();
+    option1Elem.removeEventListener("click", optionClick1);
+    option2Elem.removeEventListener("click", optionClick2);
+    infoElem.classList.add("bounceOut");
+    infoElem.addEventListener("webkitAnimationEnd", reBounce);
+}
+
+function reBounce() {
+    infoElem.classList.remove("bounceOut");
+    infoElem.removeEventListener("webkitAnimationEnd", reBounce);
+    getInputs();
 }
