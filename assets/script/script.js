916d99547352eb3079c8a3496e4702de690b41ba
Greg Mohr <gregmohr2000@yahoo.com>
2018-02-13 13:47:34 -0800

    opp AI refactor complete for logic. will merge branch

--- assets/script/script.js
+++ assets/script/script.js
@@ -20,6 +20,7 @@ x. user interactive coin toss: random player picks X or O;  opposing player stop
 x. wargames mode where both players are computer
 x. more computer techniques: starting from corner;
 x. may need to have multiple askComp sets for varied situations in the future. then just create a copy of the needed one for showAsk to splice up
+fix first move message on replay. It's fading out too quickly. may need to the way the classes are applied
 */
 let infoElem = document.getElementById("info"),
     questionElem = document.getElementById("question"),
@@ -28,8 +29,8 @@ let infoElem = document.getElementById("info"),
     player1Score = document.getElementById("player1-score"),
     player2Score = document.getElementById("player2-score"),
     playerInfo = [...document.getElementsByClassName("player-info")],
-    allSpaces = {},
-    availableSpaces = {},
+    spaces = [...document.getElementsByClassName("space")],
+    availableSpaces = [],
     player1 = new Player(1, "human"),
     player2 = new Player(2),
     ask,
@@ -50,10 +51,9 @@ let infoElem = document.getElementById("info"),
     corners = ["space1", "space3", "space7", "space9"],
     edges = ["space2", "space4", "space6", "space8"],
     curPlayer,
-    move = () => { return player1.moves.length + player2.moves.length },
-    lastMove = () => { return player1.moves[player1.moves.length - 1] };
+    move = () => { return player1.moves.length + player2.moves.length };
 
-initOnce();
+// initOnce();
 init();
 showAsk();
 
@@ -73,41 +73,14 @@ function Ask(question, option1, option2, result1, result2) {
 }
 
 function initOnce() {
-    let spaces = [...document.getElementsByClassName("space")];
-    let corners = [
-        [spaces[8]],
-        [spaces[6], spaces[8]],
-        [spaces[6]],
-        [spaces[2], spaces[8]],
-        [],
-        [spaces[0], spaces[6]],
-        [spaces[2]],
-        [spaces[0], spaces[2]],
-        [spaces[0]]
-    ];
-    for (let id = 1; id <= 9; id++) {
-        allSpaces["space" + id] = {
-            oppCorners: corners[id - 1],
-            elem: spaces[id - 1],
-        };
-    }
     //refactor: if I decide to split the top variables into objects, they'll init here
 }
 
 function init() {
-    console.log("allSpaces");
-    console.dir(allSpaces);
-    availableSpaces = Object.assign({}, allSpaces);
-    console.log("availableSpaces");
-    console.dir(availableSpaces);
+    availableSpaces = ["space1", "space2", "space3", "space4", "space5", "space6", "space7", "space8", "space9"];
 }
 
 function showAsk() {
-    if (!askComponents.length) {
-        console.log("no more questions, choosing first player");
-        chooseFirst();
-        return;
-    }
     ask = new Ask(...askComponents.shift());
     console.log("showing ask");
     questionElem.innerHTML = ask.question;
@@ -115,7 +88,7 @@ function showAsk() {
     option2Elem.innerHTML = ask.option2;
     option1Elem.addEventListener("click", optionClick1 = function() { optionClick(ask.result1) }); //*** Intentional assignment ***//
     option2Elem.addEventListener("click", optionClick2 = function() { optionClick(ask.result2) }); //*** Intentional assignment ***//
-    console.log("exiting show ask");
+    console.log("waiting for response");
 }
 
 function optionClick(callback) {
@@ -130,24 +103,24 @@ function optionClick(callback) {
 function reBounceIn() {
     infoElem.classList.remove("bounceOut");
     infoElem.removeEventListener("webkitAnimationEnd", reBounceIn);
-    console.log("re-calling showAsk");
-    showAsk();
+    checkQuestions();
+}
+
+function checkQuestions() {
+    if (askComponents.length > 0) {
+        console.log("re-calling showAsk");
+        showAsk();
+    } else {
+        console.log("no more questions, choosing first player");
+        chooseFirst();
+    }
 }
 
 function chooseFirst() {
     let outcome = Math.floor(Math.random() * 2) + 1;
     infoElem.innerHTML = "Player " + outcome + " goes first.";
     curPlayer = outcome == 1 ? player1 : player2;
-    // curPlayer = player1;
-    // infoElem.innerHTML = "Player 1 goes first.";
-
-    setTimeout(gameStart, 2000);
-    for (let el in availableSpaces) {
-        availableSpaces[el].elem.addEventListener("mousedown", spaceClick);
-    }
-    if (curPlayer.type == "computer") { //this might move to gameStart
-        computerMove();
-    }
+    setTimeout(gameStart, 1000);
 }
 
 function gameStart() {
@@ -156,185 +129,142 @@ function gameStart() {
         el.removeAttribute("hidden");
         el.classList.add("fadeIn");
     });
+    spaces.forEach(el => {
+        el.addEventListener("mousedown", spaceClick);
+    });
+    if (curPlayer.type == "computer") {
+        computerMove();
+    }
 }
 
 function computerMove() {
-    console.log("computerMove");
     let compMove;
     findMove: {
-            //*** All below compMove assignments intentional ***//
-            if (move() >= 4) {
-                if ((compMove = canWin(player2.moves))) break findMove; //move 5+
-                if ((compMove = canWin(player1.moves))) break findMove; //move 4+
-                if ((compMove = canFork(player1.moves))) break findMove; //move 4+
-            }
-            if (availableSpaces.space5) {
-                compMove = availableSpaces.space5.elem;
-                break findMove;
-            }
-            console.log("lastMove");
-            console.log(lastMove());
-            console.log(player1.moves[player1.moves.length - 1]);
-            if ((compMove = farCorner(lastMove()))) break findMove;
-            if (corners) {
-                compMove = corners[0];
-                break findMove;
-            }
-            compMove = edges[0];
+        //*** All below compMove assignments intentional ***//
+        if (move() >= 3) {
+            if ((compMove = canWin(player2.moves))) break findMove; //move 5+
+            if ((compMove = canWin(player1.moves))) break findMove; //move 3+
+            if ((compMove = canFork(player1.moves))) break findMove; //move 4+
         }
-        //all functions currently take id or array of ids as input
-        //make sure all functions return an id and grab the corresponding spaces element here
-        //allSpaces and availableSpaces may end up being just ids too
-        //if compMove is null console.log("no more moves, how did we get here sithout win/draw")
+        compMove = bestOpen();
+    }
+    console.log("computer clicks: " + compMove);
+    compMove = spaces.find(el => { return el.id == compMove });
     compMove.dispatchEvent(new MouseEvent('mousedown'));
 }
-
-function farCorner(checkSpace) {
-    let compMove;
-    console.log("farCorner checkSpace");
-    console.dir(checkSpace);
-    // if (checkSpace.oppCorners.length === 1 && availableSpaces[checkSpace.oppCorners[0]]) return availableSpaces[checkSpace.oppCorners[0]];
-}
-
-function anyCorner() {
-
-}
-
-function anyEdge() {
-
-}
-
-function compCenter() {
-    spaces[4].dispatchEvent(new MouseEvent('mousedown'));
-}
-
-// function compOppCorner() {
-//     let compMove;
-//     console.log("last move: " + lastMove());
-//     console.dir(player1.moves);
-//     switch (lastMove()) {
-//         case "space1":
-//             if (availableSpaces.includes("space9")) compMove = spaces[8];
-//             break;
-//         case "space2":
-//             if (["space7", "space9"].some(sp => availableSpaces.includes(sp))) compMove = availableSpaces.includes("space7") ? spaces[6] : spaces[8];
-//             break;
-//         case "space3":
-//             if (availableSpaces.includes("space7")) compMove = spaces[6];
-//             break;
-//         case "space4":
-//             if (["space3", "space9"].some(sp => availableSpaces.includes(sp))) compMove = availableSpaces.includes("space3") ? spaces[2] : spaces[8];
-//             break;
-//         case "space6":
-//             if (["space1", "space7"].some(sp => availableSpaces.includes(sp))) compMove = availableSpaces.includes("space1") ? spaces[0] : spaces[6];
-//             break;
-//         case "space7":
-//             if (availableSpaces.includes("space3")) compMove = spaces[2];
-//             break;
-//         case "space8":
-//             if (["space1", "space3"].some(sp => availableSpaces.includes(sp))) compMove = availableSpaces.includes("space1") ? spaces[0] : spaces[2];
-//             break;
-//         case "space9":
-//             if (availableSpaces.includes("space1")) compMove = spaces[0];
-//             break;
-//         default:
-//             console.log("could not find open opposite corner for: " + lastMove());
-//             console.log("choosing first available");
-//     }
-//     console.log("compMove: " + compMove);
-//     compMove = compMove || document.getElementById(availableSpaces[0]);
-//     console.log("compMove: " + compMove.id);
-
-//     compMove.dispatchEvent(new MouseEvent('mousedown'));
-// }
-
-// function compWinBlockCorner() {
-//     let winMove = canWin(player2);
-//     if (winMove != false) { //if win exists, take it
-//         winMove.dispatchEvent(new MouseEvent('mousedown'));
-//         return;
-//     }
-//     winMove = canWin(player1);
-//     if (winMove != false) { //if player1 can win on next turn, block
-//         winMove.dispatchEvent(new MouseEvent('mousedown'));
-//         return;
-//     }
-//     compOppCorner();
-// }
 
 function canWin(moves) {
     let found = [],
         missing = [];
-
     for (let i = 0; i < winCombos.length; i++) {
-        console.log("win combo: " + winCombos[i]);
-
         winCombos[i].forEach(sp => {
             if (moves.includes(sp)) {
-                console.log("found: " + sp);
                 found.push(sp);
             } else {
-                console.log("missing: " + sp);
                 missing.push(sp);
             }
         });
 
-        if (found.length === 2 && availableSpaces.includes("" + missing)) {
-            console.log("found: " + found);
-            console.log("missing: " + missing);
-            console.log("is missing available? " + availableSpaces.includes(missing));
-            console.dir(availableSpaces);
-            winningSpace = spaces.find(sp => sp.id == missing);
-            return winningSpace;
-        }
+        if (found.length === 2 && availableSpaces.includes("" + missing)) return missing;
+
         found.length = 0;
         missing.length = 0;
     }
-    // return false;
 }
 
 function canFork(moves) {
-    //for every availableSpace, add to copy of player moves, run canWin(player1.moves) modification
-    //that finds all matches and returns space or false, not just first and true/false
-    //if it finds 2 on 1 board, there's a fork to block
+    //may be able to move this first check to the end to check count > 3, take first open edge, 
+    if (move() == 3 && (moves.includes("space1") && moves.includes("space9") || (moves.includes("space3") && moves.includes("space7")))) {
+        if (availableSpaces.some(el => { return edges.includes(el) })) {
+            return availableSpaces.find(el => { return edges.includes(el); });
+        }
+    }
+    for (let i = 0; i < availableSpaces.length; i++) {
+        let count = 0;
+        let checkMoves = moves.concat();
+        checkMoves.push(availableSpaces[i]);
 
+        let found = [],
+            missing;
+
+        winCombos.forEach(combo => {
+            combo.forEach(sp => {
+                if (checkMoves.includes(sp)) {
+                    found.push(sp);
+                } else {
+                    missing = sp;
+                }
+            });
+
+            if (found.length === 2 && availableSpaces.includes(missing)) count++;
+
+            found.length = 0;
+            missing.length = 0;
+        });
+
+        if (count > 1) return availableSpaces[i];
+    }
+}
+
+function bestOpen() {
+    let compMove;
+    findOpen: {
+        //break into smaller functions?
+        //center open?
+        if (availableSpaces.includes("space5")) {
+            compMove = "space5";
+            break findOpen;
+        }
+        //farcorner open?
+        if (move() > 0) {
+            let lastMove = player1.moves[player1.moves.length - 1];
+            let elem = spaces.find(el => { return el.id == lastMove });
+            let spaceCorners = JSON.parse(elem.dataset.farcorners);
+
+            spaceCorners.some(el => {
+                if (availableSpaces.includes(el)) {
+                    compMove = el;
+                    return el;
+                }
+            })
+            if (compMove) break findOpen;
+        }
+        //any corner open?
+        if (availableSpaces.some(el => { return corners.includes(el) })) {
+            compMove = availableSpaces.find(el => { return corners.includes(el); })
+            break findOpen;
+        }
+        //only edges open
+        compMove = availableSpaces[0];
+    }
+    return compMove;
 }
 
 function spaceClick() {
     console.log("+++++++++spaceClick on: " + this.id);
     this.removeEventListener("mousedown", spaceClick);
 
-    // availableSpaces.splice(availableSpaces.indexOf(this.id), 1);
-    delete availableSpaces[this.id];
-    console.log("availableSpaces/allSpaces");
-    console.dir(availableSpaces);
-    console.dir(allSpaces);
-
-    let mark = document.getElementById(this.id);
-    mark.classList.add(curPlayer.marker);
-
+    let removed = availableSpaces.splice(availableSpaces.indexOf(this.id), 1);
     curPlayer.moves.push(this.id);
+    this.classList.add(curPlayer.marker);
+
     console.log("move#: " + move());
-    if (move() > 4) { //can I make this more eloquent?
-        if (endCheck()) {
-            return;
-        }
-    }
 
+    if (move() > 4 && endCheck()) return;
+
+    //should I split this into a seperate nextPlayer function?
     curPlayer = curPlayer === player1 ? player2 : player1;
-
     if (curPlayer.type === "computer") {
         computerMove();
     }
 }
 
 function endCheck() {
-    console.log("endcheck");
+    console.log("endCheck");
     let win = winCombos.some(el => {
-        // console.log("win combo");
-        // console.dir(el);
         return el.every(sp => { return curPlayer.moves.includes(sp) });
     });
+    //some of this logic makes more sense in the gameEnd function. Maybe this should just check for win/draw and pass the result forward
     console.log("win check: " + win);
     if (win || move() === 9) {
         console.log("ending game");
@@ -347,23 +277,19 @@ function endCheck() {
             infoElem.innerHTML = "Draw";
         }
         infoElem.classList.remove("fadeOut");
-        setTimeout(gameEnd, 3000);
+        setTimeout(gameEnd, 1000);
         return true;
     }
-    // else {
-    //     return false;
-    // }
 }
 
 function gameEnd() {
+    //most of this stuff should probably move to a cleanup function and gameEnd should handle messaging regarding game outcome
     infoElem.classList.add("fadeOut");
     console.log("removing markers");
     spaces.forEach(el => el.classList.remove("x", "o"));
     curPlayer = null;
     player1.moves.length = 0;
     player2.moves.length = 0;
-    // while (pattern.length > 0) pattern.shift();
-    pattern = [];
     init();
     chooseFirst();
 }
